d31b459b5d02a5900dc775d0860a7fc40d857bf4
Paulo Dantas <paulo.dantas.solutic@petrobras.com.br>
2021-02-12 16:06:38 -0300

    notFound page

--- components/Search.js
+++ components/Search.js
@@ -1,7 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+import { AiOutlineSearch } from 'react-icons/ai'
 
 const Search = () => {
-  return <div>search</div>
+  const [user, setUser] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
+  return (
+    <section>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <AiOutlineSearch />
+          <input
+            type='text'
+            placeholder='Entre com o nome de usuario'
+            value={user}
+            onChange={(e) => setUser(e.target.value)}
+          />
+          <button type='submit'>search</button>
+        </div>
+      </form>
+    </section>
+  )
 }
 
 export default Search
---
+++ pages/404.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import styled from 'styled-components'
+import Link from 'next/link'
+
+const NotFound = () => {
+  return (
+    <Wrapper>
+      <div>
+        <h1>404</h1>
+        <h3>Desculpe, a página que você tentou acessar não existe.</h3>
+        <Link href='/' className='btn'>
+          <Button>Voltar</Button>
+        </Link>
+      </div>
+    </Wrapper>
+  )
+}
+const Wrapper = styled.section`
+  min-height: 100vh;
+  display: grid;
+  place-items: center;
+  background: var(--clr-primary-10);
+  text-align: center;
+  h1 {
+    font-size: 10rem;
+  }
+  h3 {
+    color: var(--clr-grey-3);
+    margin-bottom: 1.5rem;
+  }
+`
+const Button = styled.button`
+  text-transform: uppercase;
+  width: 40%;
+  padding: 20px;
+  background-color: var(--clr-primary-4);
+  color: var(--clr-primary-10);
+  border: none;
+  border-radius: 10px;
+  &:hover {
+    transition: all 0.2s ease-in-out;
+    opacity: 0.7;
+  }
+`
+export default NotFound
